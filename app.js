bb998561ad36c004dc1f6df9dcf4a963736d36ef
Sarmistha Garg <sarmisthagarg123@gmail.com>
2021-09-06 18:45:49 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Selectors
+const preloader = document.getElementById("loader");
+
+// Events
+window.addEventListener("load", loader);
+
+// Functions
+function loader() {
+  preloader.style.display = "none";
+}
